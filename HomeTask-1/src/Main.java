09d01a1618215bae360f0407dcff9df709300005
Armen Baghunts <gphbakarm@gmail.com>
2026-01-09 02:47:10 +0400

    hometask-1 is committed

---
+++ HomeTask-1/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=5;
+        b=8;
+        c=4;
+        float k;
+        char ch='8';
+        double result=Math.pow(a+b,3);
+        k=(a+b+c)/3f-(a+b+c)/3;
+        System.out.println((a+b+c)/3f);
+        System.out.println(100*a+10*b+c);
+        System.out.println(k);
+        System.out.println((int) ch -48);
+        System.out.println(result);
+    }
+}
+
